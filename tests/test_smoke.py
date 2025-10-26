5101ea4def4696451869fad51dff4f24747220ba
Trinh Pham <phkhanhtrinh23@gmail.com>
2025-10-26 12:02:17 +1000

    init commit

--- tests/test_smoke.py
+++
@@ -1,7 +0,0 @@
-from fusion_evaluator.metrics.fusion import fusion_score, FusionWeights
-
-
-def test_fusion_score_bounds():
-	w = FusionWeights()
-	assert 0.0 <= fusion_score(0.0, 0.0, 0.0, w) <= 1.0
-	assert 0.0 <= fusion_score(1.0, 1.0, 1.0, w) <= 1.0
